2d8261b11bfcea02677804febd5e26455db3ca6f
Yuriy Zhmurko <777zhm@gmail.com>
2019-02-07 19:08:58 +0200

    colors

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<?php the_content(); ?>
 	<?php endwhile; ?>
-	<p>sftp - works good!</p>
+	
 	<?php endif; ?>
 
 <?php get_footer(); ?>
