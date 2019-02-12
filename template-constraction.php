e00959a24f5ae5d706a9677d002fb64ae3e77a64
Yuriy Zhmurko <777zhm@gmail.com>
2019-02-12 14:29:31 +0200

    partners logo

---
+++ template-constraction.php
@@ -0,0 +1,14 @@
+<?php /* Template Name: Under constraction page ukr Template */ ?>
+
+
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<h2 class="entry-title">Вибачте, сайт знаходиться в процесі розробки.</h2>
+				<h2 class="entry-title">Але, зовсім скоро, ми повернемось оновленими.</h2>
+			</div>
+		</div>
+	</div>
+</section>
+
