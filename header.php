b1b520e2a1a610b63e4f68bca27eb1c6d93ccd7f
Yuriy Zhmurko <777zhm@gmail.com>
2019-02-17 13:27:08 +0200

    title in header

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 	
-	<title><?php wp_title(); ?></title>
+	<title><?php bloginfo( 'name' ); echo " | "; the_title(); ?></title>
 	<?php wp_head(); ?>
 </head>
 
