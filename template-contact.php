9e4b47c9fbf6cd9d4ed4865cdc2500567ef7eb24
Yuriy Zhmurko <777zhm@gmail.com>
2019-02-27 19:35:58 +0200

    accordion deleted

--- template-contact.php
+++ template-contact.php
@@ -3,8 +3,8 @@
 
 <section class="contact_page_section">
 	<div class="container">
-		<div class="row">
-			<div class="col-lg-3">
+		<div class="row contact_page_row">
+			<div class="col-lg-2 col-md-4 col-sm-12">
 				<div class="contact_tel">
 					<h4>Телефон</h4>
 					<p><a href="tel:+380442289601">+38 (044) 228 96 01</a></p>
@@ -26,8 +26,8 @@
 					<p>Обід: 13.00 - 14.00</p>
 				</div>
 			</div>
-			<div class="col-lg-9">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2541.1160789600235!2d30.496578815730928!3d50.43893847947378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cef1c6cb39e5%3A0x258681f2cb3b1854!2sEurasia%2C+Zhylianska+St%2C+75%2C+Kyiv%2C+02000!5e0!3m2!1sen!2sua!4v1550179914993" width="800" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+			<div class="map_block col-lg-10 col-md-8 col-sm-12">
+				<iframe  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2541.1160789600235!2d30.496578815730928!3d50.43893847947378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cef1c6cb39e5%3A0x258681f2cb3b1854!2sEurasia%2C+Zhylianska+St%2C+75%2C+Kyiv%2C+02000!5e0!3m2!1sen!2sua!4v1550179914993" width="100%" height="450" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" allowfullscreen></iframe>
 			</div>
 		</div>
 	</div>
